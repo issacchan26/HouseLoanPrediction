ec04c93aa37380eff057f9f38d0427d78b3cff03
issac <issac1996_chan@yahoo.com.hk>
2023-09-05 10:08:07 -0400

    added test.py

---
+++ test.py
@@ -0,0 +1,71 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from sklearn.model_selection import train_test_split
+import pandas as pd
+from model import MLP
+from torch.utils.data import TensorDataset, DataLoader
+from torch.utils.tensorboard import SummaryWriter
+
+path = 'path to train_data.csv'
+model_path = 'path to your state_dict model'
+
+def read_csv(path):
+    df = pd.read_csv(path, on_bad_lines='skip')
+    X = df.drop(['TARGET'], axis=1).values
+    y = df['TARGET'].values
+    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=3)
+
+    X_train = torch.FloatTensor(X_train)
+    X_test = torch.FloatTensor(X_test)
+    y_train = torch.FloatTensor(y_train)
+    y_test = torch.FloatTensor(y_test)
+
+    return X_train, X_test, y_train, y_test
+
+_, X_test, _, y_test = read_csv(path)
+test_tensor = TensorDataset(X_test, y_test) 
+test_loader = DataLoader(dataset = test_tensor, batch_size = 1)
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model = MLP(20,1).to(device)
+model.load_state_dict(torch.load(model_path))
+criterion = nn.BCELoss()
+
+acc = 0
+val_loss = 0
+total_0 = 0
+total_1 = 1
+tar_0 = 0
+tar_1 = 0
+
+model.eval()
+with torch.no_grad():
+    for X_val, y_val in test_loader:
+        X_val, y_val = X_val.to(device), y_val.to(device)
+        y_pred = model(X_val)
+        loss = criterion(y_pred, y_val.unsqueeze(1)) 
+        val_loss += loss
+
+        if y_val == 0:
+            total_0 += 1
+            if y_pred.round() == y_val:
+                tar_0 += 1
+                acc += 1
+
+        elif y_val == 1:
+            total_1 += 1
+            if y_pred.round() == y_val:
+                tar_1 += 1
+                acc += 1
+
+acc = acc/len(test_loader)
+val_loss = val_loss/len(test_loader)
+
+print('total number of class 0:', total_0)
+print('correct number of class 0:', tar_0)
+print('total number of class 1:', total_1)
+print('correct number of class 1:', tar_1)
+print('accuracy:', acc)
+print('mean loss:', val_loss.item())
+
